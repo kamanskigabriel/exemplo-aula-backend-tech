c2976926b575cd702b4fe88b5e06eb073d6f9415
gabriel <gabrielkamanski0gmail.com>
2026-08-06 22:12:19 -0300

    Pre-prova-finalizado

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import router from './router/carro.js'
+
+const app = express()
+
+app.use(express.json())
+app.use("/api/v1/carro", router)
+
+app.listen(3000, ()=> {
+    console.log("Servidor ouvindo na porta 3000")
+})
